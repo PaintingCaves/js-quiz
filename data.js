cec26d32b1e52124c08d72798ad0d930fb92aaf8
Nicholas <nicholasmazower@gmail.com>
2018-04-20 14:55:59 +0200

    Finished question content

--- data.js
+++ data.js
@@ -32,7 +32,113 @@ let questions = [{
     questionBody: 'What tools were on the Soviet flag?',
     answers: ['Hammer and Sickle', 'Spade and Rake', 'Trowel and Hoe', 'Tractor and Bakkie'],
     correctAnswer: 'Hammer and Sickle'
+  },
+  {
+    title: 'part6',
+    questionNumber: 6,
+    questionBody: 'The mutiny of which ship  is viewed as leading to the revolution?',
+    answers: ['Dreadnought', 'Enterprise', 'Kursk', 'Potemkin'],
+    correctAnswer: 'Potemkin'
+  },
+  {
+    title: 'part7',
+    questionNumber: 7,
+    questionBody: 'In which what year did this mutiny take place?',
+    answers: ['1905', '1939', '1917', '1919'],
+    correctAnswer: '1905'
+  },
+  {
+    title: 'part8',
+    questionNumber: 8,
+    questionBody: 'During which war did the revolution take place?',
+    answers: ['First World War', 'Second World War', 'Napoleonic Wars', 'Korean War'],
+    correctAnswer: 'First World War'
+  },
+  {
+    title: 'part9',
+    questionNumber: 9,
+    questionBody: 'What did USSR stand for?',
+    answers: ['Union of Slavic Socialist Republics', 'United Supreme Socialists Rave', 'United States of South Russia', 'Union of Soviet Socialist Republics'],
+    correctAnswer: 'Union of Soviet Socialist Republics'
+  },
+  {
+    title: 'part10',
+    questionNumber: 10,
+    questionBody: 'By what name were the revolutionary forces known?',
+    answers: ['Pink Army', 'Red Army', 'White Army', 'People\'s Liberation Army'],
+    correctAnswer: 'Red Army'
+  },
+  {
+    title: 'part11',
+    questionNumber: 11,
+    questionBody: 'Which Tsar was executed during the revolution?',
+    answers: ['Peter the Great', 'Catherine', 'Nicholas II', 'Ivan'],
+    correctAnswer: 'Nicholas II'
+  },
+  {
+    title: 'part12',
+    questionNumber: 12,
+    questionBody: 'What was the name of the famous mystic who was reputed to be nigh unkillable?',
+    answers: ['Rasmussen', 'Raslenin', 'Rastalin', 'Rasputin'],
+    correctAnswer: 'Rasputin'
+  },
+  {
+    title: 'part13',
+    questionNumber: 13,
+    questionBody: 'Who ended up as the leader of the USSR?',
+    answers: ['Lenin', 'Stalin', 'Trotsky', 'Churchill'],
+    correctAnswer: 'Stalin'
+  },
+  {
+    title: 'part14',
+    questionNumber: 14,
+    questionBody: 'Which of these Nations did not aid the White Army?',
+    answers: ['British Empire', 'Japanese Empire', 'USA', 'Germany'],
+    correctAnswer: 'Germany'
+  },
+  {
+    title: 'part15',
+    questionNumber: 15,
+    questionBody: 'What was the pejorative name given to wealthy peasants during the revolution?',
+    answers: ['Kulaks', 'Streloks', 'Cheeki Breeki', 'Katyusha'],
+    correctAnswer: 'Kulaks'
+  },
+  {
+    title: 'part16',
+    questionNumber: 16,
+    questionBody: 'How many revolutions were in the Russian Revolution?',
+    answers: ['One', 'Two', 'Three', 'Four'],
+    correctAnswer: 'Two'
+  },
+  {
+    title: 'part17',
+    questionNumber: 17,
+    questionBody: 'What was the city of Petrograd renamed to?',
+    answers: ['Leningrad', 'Stalingrad', 'Kiev', 'Minsk'],
+    correctAnswer: 'Leningrad'
+  },
+  {
+    title: 'part18',
+    questionNumber: 18,
+    questionBody: 'Which member of the royal family was it rumoured had escaped?',
+    answers: ['Anastasia', 'Alexandra', 'Alexei', 'Maria'],
+    correctAnswer: 'Anastasia'
+  },
+  {
+    title: 'part19',
+    questionNumber: 19,
+    questionBody: 'In which year did the USSR dissolve?',
+    answers: ['1991', '1990', '1989', 'Still secretly running'],
+    correctAnswer: '1991'
+  },
+  {
+    title: 'part20',
+    questionNumber: 20,
+    questionBody: 'Which famous allegorical novel was written about the Russian Revolution?',
+    answers: ['1984', 'Charlotte\'s Web', 'Animal Farm', 'A Brave New World'],
+    correctAnswer: 'Animal Farm'
   }
+
 ]
 let messages = [
   "How!?",
